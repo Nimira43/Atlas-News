54cbf2a849c3cee53690f79ac7b1e2030cbc0548
Nimira43 <n.rabone72@aol.com>
2024-11-25 03:32:26 +0000

    Started jsx code for news.app, logo and navbar

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,7 +1,25 @@
 const News = () => {
   return (
-    <div>
-      <h1>News Page</h1>
+    <div className='news-app'>
+      <div className='news-header'>
+        <h1 className='logo'>Global News</h1>
+      </div>
+      <div className="news-content">
+        <nav className="navbar">
+          <h1 className='nav-heading'>Categories</h1>
+          <div className="categories">
+            <a href="#" className="nav-link">General</a>
+            <a href="#" className="nav-link">World</a>
+            <a href="#" className="nav-link">Entertainment</a>
+            <a href="#" className="nav-link">Sport</a>
+            <a href="#" className="nav-link">Technology</a>
+            <a href="#" className="nav-link">Business</a>
+            <a href="#" className="nav-link">Health</a>
+            <a href="#" className="nav-link">Science</a>
+            <a href="#" className="nav-link">National</a>
+          </div>
+        </nav>
+      </div>
     </div>
   )
 }
