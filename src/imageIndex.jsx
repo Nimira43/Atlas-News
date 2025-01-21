5b1491636385861e30d6b970dd21b5554b7261e1
Nimira43 <n.rabone72@aol.com>
2025-01-21 11:40:18 +0000

    Created imagesIndex.jsx to handle image imports

---
+++ src/imageIndex.jsx
@@ -0,0 +1,14 @@
+import Entertainment from './images/entertainment.jpg'
+import Health from './images/health.jpg'
+import Sport from './images/sport.jpg'
+import Science from './images/science.jpg'
+import Demo from './images/demo.jpg'
+import National from './images/national.jpg'
+import NoImage from './images/no-img.png'
+import Tech1 from './images/tech-1.jpg'
+import Tech2 from './images/tech-2.jpg'
+import Tech3 from './images/tech-3.jpg'
+import Tech4 from './images/tech-4.jpg'
+import World from './images/world.jpg'
+
+export {Entertainment, Health, Sport, Science, Demo, Science, National, NoImage, World, Tech1, Tech2, Tech3, Tech4}
