89a0b280bdb79fa85a07150c6383270e2d10c0e2
Nimira43 <n.rabone72@aol.com>
2025-04-29 09:43:37 +0100

    News section sub container styled. Tweaks made elsewhere.

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -15,7 +15,7 @@ const News = () => {
           <div className='news-grid'>News Grid</div>
         </div>
         <div className='my-blogs'>My Blogs</div>
-        <div className="weather-calendar">
+        <div className='weather-calendar'>
           <Weather />
           <Calendar />
         </div>
