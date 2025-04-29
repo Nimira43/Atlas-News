1ce58f73dd77f04a31f5218b66234141d216f9a5
Nimira43 <n.rabone72@aol.com>
2025-04-29 07:14:54 +0100

    Added logo class

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <h1 className>Nova News</h1>
+      <h1 className='logo'>Nova News</h1>
       <hr />
       <p>by NimiraTech</p>
     </div>
