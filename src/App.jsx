00c2b66f9a59471673565d10fb624313c6f5cdca
Nimira43 <n.rabone72@aol.com>
2025-04-29 07:24:21 +0100

    Created components/News.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import News from "./components/News"
 
-function App() {
+const App = () => {
   return (
     <div>
-      <h1 className='logo'>Nova News</h1>
-      <hr />
-      <p>by NimiraTech</p>
+      <News />
     </div>
   )
 }
 
 export default App
+
 
---
+++ src/components/News.jsx
@@ -0,0 +1,9 @@
+const News = () => {
+  return (
+    <div>
+      <h1>Nova News</h1>
+    </div>
+  )
+}
+
+export default News
