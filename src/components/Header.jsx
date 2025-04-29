17dba74860afebdcb62bd2d702524a6651b304fd
Nimira43 <n.rabone72@aol.com>
2025-04-29 20:10:48 +0100

    Header markup done.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,21 @@
+import { PiMagnifyingGlassLight } from 'react-icons/pi'
+
 const Header = () => {
   return (
-    <header className='news-header'>News Header</header>
+    <header className='news-header'>
+      <h1 className='logo'>Nova News</h1>
+      <div className='search-bar'>
+        <form>
+          <input
+            type="text"
+            placeholder="Search"
+          />
+          <button type='submit'>
+            <PiMagnifyingGlassLight />
+          </button>
+        </form>
+      </div>
+    </header>
   )
 }
 
