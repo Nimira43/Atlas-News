a14d25256b19052293a3e2c1101f19785929e277
Nimira43 <n.rabone72@aol.com>
2025-04-29 08:05:31 +0100

    Tidy up

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,20 +1,20 @@
-import Calendar from "./Calendar"
-import Weather from "./Weather"
+import Calendar from './Calendar'
+import Weather from './Weather'
 
 const News = () => {
   return (
     <div className='news'>
       <header className='news-header'>News Header</header>
-      <div className="news-content">
-        <div className="navbar">
-          <div className="user">User</div>
-          <nav className="categories">Categories</nav>
+      <div className='news-content'>
+        <div className='navbar'>
+          <div className='user'>User</div>
+          <nav className='categories'>Categories</nav>
         </div>
-        <div className="news-section">
-          <div className="headline">Headline</div>
-          <div className="news-grid">News Grid</div>
+        <div className='news-section'>
+          <div className='headline'>Headline</div>
+          <div className='news-grid'>News Grid</div>
         </div>
-        <div className="my-blogs">My Blogs</div>
+        <div className='my-blogs'>My Blogs</div>
         <Weather />
         <Calendar />
       </div>
