5971a2eb71a8f949fb3c92771736dfc8582ae240
Nimira43 <n.rabone72@aol.com>
2025-04-30 03:29:56 +0100

    Brought and loaded User image.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
+import User from '../images/User.jpg'
+
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <div className='user bg-box'>User</div>
+      <div className='user bg-box'>
+        <img src={User} alt="user" />
+        <p>Jim's Blog</p>
+      </div>
       <nav className='categories bg-box'>Categories</nav>
     </div>
   )
