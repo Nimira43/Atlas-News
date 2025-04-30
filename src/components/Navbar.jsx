ffdaef2100b1cf3e9ec98ef85ce35cff0ed45474
Nimira43 <n.rabone72@aol.com>
2025-04-30 03:37:39 +0100

    Setting up Categories navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,47 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <div className='user bg-box'>
-        <img src={User} alt="user" />
+        <img
+          src={User}
+          alt="user"
+        />
         <p>Jim's Blog</p>
       </div>
-      <nav className='categories bg-box'>Categories</nav>
+      <nav className='categories bg-box'>
+        <h1 className='nav-heading'>Categories</h1>
+        <div className='nav-links'>
+          <a href="#" className='nav-link'>
+            Top News
+          </a>
+          <a href="#" className='nav-link'>
+            UK 
+          </a>
+          <a href="#" className='nav-link'>
+            World
+          </a>
+          <a href="#" className='nav-link'>
+            Sport
+          </a>
+          <a href="#" className='nav-link'>
+            Technology
+          </a>
+          <a href="#" className='nav-link'>
+            Business
+          </a>
+          <a href="#" className='nav-link'>
+            Entertainment
+          </a>
+          <a href="#" className='nav-link'>
+            Health
+          </a>
+          <a href="#" className='nav-link'>
+            Science
+          </a>
+          <a href="#" className='nav-link'>
+            Featured
+          </a>
+        </div>
+      </nav>
     </div>
   )
 }
